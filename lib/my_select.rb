20d5da2bef70ded48d8b2ef31c6cdbbb7e90dd27
Kyle Graden <kylegraden3@gmail.com>
2019-03-12 07:56:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  new_collection = [];
  i = 0;
  while i < collection.length do;
-   new_collection.include?(collection) yield(collection[i]);
+   new_collection.include? yield(collection[i]);
    i += 1
  end
  new_collection
